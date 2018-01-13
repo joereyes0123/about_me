0ac87803b01b5d7f26af7c3a6d91c85e556887bd
Rey0123 <joereyes0123@yahoo.com>
2018-01-13 15:10:36 -0800

    modified alert in questions to reflect user answer. Also added gitnore

--- js/app.js
+++ js/app.js
@@ -6,78 +6,78 @@
 var NumCount = 0;
 var userName = prompt('what is your name?');
 
-var rightWrongArray = ['that is correct', 'WRONG'];
+var rightWrongArray = [' that is Correct', ' Incorrect '];
 
 //Question 1: Asks hair question color
 function question1() {
   var userHairC = prompt('Is my hair color Brown?, Yes or No?').toLowerCase();
 
   if(userHairC === 'yes' || userHairC === 'y'){
-    alert(rightWrongArray[0]);
+    alert('RIGHT!! You answered ' + '"' + userHairC + '"' + rightWrongArray[0]);
     NumCount++;
   }else{
-    alert(rightWrongArray[1]);
+    alert('WRONG!! You answered ' + '"' + userHairC + '" that is ' + rightWrongArray[1]);
   }
   console.log('User guessed hair color as ' + userHairC);
 }
 
-question1();
+
 
 //Question 2: Asks pets question color
 function question2(){
   var pets = parseInt(prompt('Do I own any pets?, Yes or No').toLowerCase());
-  if(pets === 'yes' || pets === 'y'){
-    alert(rightWrongArray[0]);
+  if(pets === 'no' || pets === 'n'){
+    alert('RIGHT!! You answered ' + '"' + pets + '"' + rightWrongArray[0]);
     NumCount++;
   }else{
-    alert(rightWrongArray[1]);
+    alert('WRONG!! You answered ' + '"' + pets + '" that is ' + rightWrongArray[1]);
   }
   console.log('user guessed pets question as' + pets);
 }
 
-question2();
+
 
 // //Question 3: Asks hand question color
 function question3() {
   var userHand = prompt('Am I left handed?, Yes or No').toLowerCase();
   if(userHand === 'no' || userHand === 'n'){
-    alert(rightWrongArray[0]);
+    alert('RIGHT!! You answered ' + '"' + userHand + '"' + rightWrongArray[0]);
     NumCount++;
   }else{
-    alert(rightWrongArray[1]);
+    alert('WRONG!! You answered ' + '"' + userHand + '" that is ' + rightWrongArray[1]);
   }
   console.log('user answer hand dominance as' + userHand);
 }
 
-question3();
+
 
 // //Question 4: Asks eye color question
 function question4() {
   var userEyeColor = prompt('Are my eyes Brown, Yes or No ').toLowerCase();
   if(userEyeColor === 'yes' || userEyeColor === 'y'){
-    alert(rightWrongArray[0]);
+    alert('RIGHT!! You answered ' + '"' + userEyeColor + '"' + rightWrongArray[0]);
     NumCount++;
   }else{
-    alert(rightWrongArray[1]);
+    alert('WRONG!! You answered ' + '"' + userEyeColor + '" that is ' + rightWrongArray[1]);
   }
   console.log('user guessed eye color' + userEyeColor);
 }
 
-question4();
+
 
 // //Question 5: Asks ethnic background question
 function question5() {
   var userEthnic = prompt('Am I a white male ').toLowerCase();
   if(userEthnic === 'no' || userEthnic === 'n'){
-    alert(rightWrongArray[0]);
+    alert('RIGHT!! You answered ' + '"' + userEthnic + '"' + rightWrongArray[0]);
     NumCount++;
   }else{
-    alert(rightWrongArray[1]);
+    alert('WRONG!! You answered ' + '"' + userEthnic + '" that is ' + rightWrongArray[1]);
   }
   console.log('user guessed ethnic background as' + userEthnic);
 }
 
-question5();
+
 
 // //Question 6: Asks Kids question
 function question6() {
@@ -87,7 +87,7 @@ function question6() {
     if (guessNumber > 10){
       alert('answer is less than 10, try again');
     }else if(guessNumber === 0){
-      alert('you guess RIGHT!!! children are too expensive');
+      alert('you guessed RIGHT!!! children are too expensive');
       NumCount++;
       break;
     }else if(guessCount === 3){
@@ -99,7 +99,6 @@ function question6() {
   console.log('user guessed Hand Dominance' + guessNumber);
 }
 
-question6();
 
 // //Question 7: Asks favorite countries question
 function question7() {
@@ -123,6 +122,12 @@ function question7() {
   console.log('user guessed favorite country as' + favCountries);
 }
 
+question1();
+question2();
+question3();
+question4();
+question5();
+question6();
 question7();
 
 alert( userName + ' you answered ' + NumCount + ' questions correctly');
